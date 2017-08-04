c4bd001402639c065d273770596e31e6a2401c05
lotusstorm <3dsss@mail.ru>
2017-08-04 20:15:16 +0300

    svg-icon-anim

--- app/js/common.js
+++ app/js/common.js
@@ -1,7 +1,6 @@
 $(document).scroll (function () {
-	$(document).scrollTop () ? ($('.header').css({'height': '50px', 'position': 'fixed'}) && $('.header__sub__menu__home',).css('top', '38px') && $('.header__sub__menu__journal__a').css('top', '38px')) : ($('.header').css({'height': '110px', 'position': 'static'}) && $('.header__sub__menu__home').css('top', '68px') && $('.header__sub__menu__journal__a').css('top', '68px'));
+	$(document).scrollTop () ? ($('.header').addClass('header_scroll') && $('.header__sub__menu__home',).addClass('menu_scroll') && $('.header__sub__menu__journal__a').addClass('menu_scroll')) : ($('.header').removeClass('header_scroll') && $('.header__sub__menu__home').removeClass('menu_scroll') && $('.header__sub__menu__journal__a').removeClass('menu_scroll'));
 });
-
 var slideNow = 1;
 var slideCount = $('#block__for__slider__wrapper').children().length;
 var slideInterval = 6000;
